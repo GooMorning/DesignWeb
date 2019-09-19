cdf4f79b83f0a829d1e1f79ed915b38436a1806b
GooMorning <max@minitry.cn>
2019-09-19 21:30:22 +0800

    Djangomini

--- DesignWeb/settings.py
+++ DesignWeb/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'DesignWeb', #我的应用
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'DesignWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DesignWeb',
+        'USER': 'DesignWeb',
+        'PASSWORD':'DesignWeb!!',
+        'HOST':'60.205.191.26',
+        'PORT':'3306'
     }
 }
 
